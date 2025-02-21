d8ffbeac4f67501aecd74837ad9e806eafff692f
João Nicolas Freitas <133971808+JoaoNicolasFreitas@users.noreply.github.com>
2025-02-21 20:27:52 -0300

    Exercício Tela de Login
    Aula 11 dando continuidade ao estudo de TailwindCss

---
+++ Exercicio Tela de Login/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Aqui você define os arquivos onde o Tailwind irá procurar as classes usadas
+    "./src/**/*.{html,js}", // Ajuste conforme sua estrutura de pastas e tipos de arquivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
